eeeb262492ba4fbd99cf058f41d6318fb8aced08
Alexey Fedorenko <Messer.ua@gmail.com>
2019-10-19 14:29:13 +0300

    add function Right side

--- dz-water.js
+++ dz-water.js
@@ -1,32 +1,68 @@
-var arr = [2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 0];
-var maxVal = arr[0];
-var maxIndex = 0;
+let arr = [2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 0];
+let maxVal = arr[0];
+let maxIndex = 0;
 for (let i = 0; i < arr.length; i++) {
   if (arr[i] > maxVal) {
     maxIndex = i;
     maxVal = arr[i];
   }
 }
+let rightArr = arr.slice(maxIndex, maxRightIndex);
+console.log(rightArr);
 // arr[maxIndex] = arr[6]
 // maxVal = 7
-var maxRightVal = 0;
+//var maxRightVal = 0;
 var maxRightIndex = 0;
 var sWaterRight = 0;
-var sStoneRight = 0; // общая площадь камней под водой
-for (let i = maxIndex + 1; i < arr.length; i++) {
-  //	console.log(i);
+ // общая площадь камней под водой
+//var firstRightVal = rightArr[0];
+var firstRightInd = rightArr.length[0];	
+
+
+function right(arr, maxIndex) {
+	console.log(arr);
+	let firstRightVal = arr[0];
+	let maxRightVal = 0;
+	let sStoneRight = 0;
+
+//	console.log(maxVal);
+	
+for (let i = 1; i < arr.length; i++) {
   if (arr[i] > maxRightVal) {
     maxRightIndex = i;
     maxRightVal = arr[i];
   }
 }
-var stoneRight = arr.slice(maxIndex + 1, maxRightIndex); // - (массив) кол-во секций (длинна под водой) между вершинами
+	console.log('1я вершина', firstRightVal );
+	console.log('2я вершина', maxRightVal );
+let stoneRight = arr.slice(1, maxRightIndex); // - (массив) кол-во секций (длинна под водой) между вершинами
+	console.log('все что под водой', stoneRight);
 
 for (let i = 0; i < stoneRight.length; i++) {
   sStoneRight += stoneRight[i];
+//	console.log(sStoneRight, stoneRight[i]);
 }
-sWaterRight = maxRightVal * stoneRight.length - sStoneRight; // Площадь воды = высота блока * кол-во блоков(длинна под водой) - площадь камней в воде
-console.log(sWaterRight);
+	console.log("Сумма камней под водой", sStoneRight);
+	if (stoneRight.length >0) {
+    sWaterRight += maxRightVal * stoneRight.length - sStoneRight; // Площадь воды = высота блока * кол-во блоков(длинна под водой) - площадь камней в воде
+	console.log('---всего воды', sWaterRight);
+	console.log("Длинна под водой" ,stoneRight.length);
+	let newArr = arr.slice(maxRightIndex, arr.length);
+		
+	console.log('навый массив',newArr);
+	 	
+	right(newArr, maxRightIndex);
+	return sWaterRight;
+
+	}
+//sWaterRight += sWaterRight;
+}
+right(rightArr, maxIndex);
+	console.log('+++++всего воды', sWaterRight);
+
+
+
+
 // --------------------- Левая часть--------------------------//
 
 var maxLeftVal = 0;
