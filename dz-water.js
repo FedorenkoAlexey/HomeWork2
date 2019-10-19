235d5bfecca707cfac3759d4e7eec4a098a9d55d
Alexey Fedorenko <messer.ua@gmail.com>
2019-10-19 20:38:12 +0300

    + funct left

--- dz-water.js
+++ dz-water.js
@@ -8,77 +8,104 @@ for (let i = 0; i < arr.length; i++) {
   }
 }
 let rightArr = arr.slice(maxIndex, maxRightIndex);
-console.log(rightArr);
+let leftArr = arr.slice(0, maxIndex + 1);
+
+console.log(leftArr);
 // arr[maxIndex] = arr[6]
 // maxVal = 7
 //var maxRightVal = 0;
 var maxRightIndex = 0;
+var maxLeftIndex = 0;
 var sWaterRight = 0;
- // общая площадь камней под водой
+var sWaterLeft = 0;
+// общая площадь камней под водой
 //var firstRightVal = rightArr[0];
-var firstRightInd = rightArr.length[0];	
-
+var firstRightInd = rightArr.length[0];
+var firstLeftInd = leftArr.length[0];
 
 function right(arr, maxIndex) {
-	console.log(arr);
-	let firstRightVal = arr[0];
-	let maxRightVal = 0;
-	let sStoneRight = 0;
+  console.log(arr);
+  let firstRightVal = arr[0];
+  let maxRightVal = 0;
+  let sStoneRight = 0;
 
-//	console.log(maxVal);
-	
-for (let i = 1; i < arr.length; i++) {
-  if (arr[i] > maxRightVal) {
-    maxRightIndex = i;
-    maxRightVal = arr[i];
+  //	console.log(maxVal);
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > maxRightVal) {
+      maxRightIndex = i;
+      maxRightVal = arr[i];
+    }
   }
-}
-	console.log('1я вершина', firstRightVal );
-	console.log('2я вершина', maxRightVal );
-let stoneRight = arr.slice(1, maxRightIndex); // - (массив) кол-во секций (длинна под водой) между вершинами
-	console.log('все что под водой', stoneRight);
+  console.log("1я вершина", firstRightVal);
+  console.log("2я вершина", maxRightVal);
+  let stoneRight = arr.slice(1, maxRightIndex); // - (массив) кол-во секций (длинна под водой) между вершинами
+  console.log("все что под водой", stoneRight);
 
-for (let i = 0; i < stoneRight.length; i++) {
-  sStoneRight += stoneRight[i];
-//	console.log(sStoneRight, stoneRight[i]);
-}
-	console.log("Сумма камней под водой", sStoneRight);
-	if (stoneRight.length >0) {
+  for (let i = 0; i < stoneRight.length; i++) {
+    sStoneRight += stoneRight[i];
+    //	console.log(sStoneRight, stoneRight[i]);
+  }
+  console.log("Сумма камней под водой", sStoneRight);
+  if (stoneRight.length > 0) {
     sWaterRight += maxRightVal * stoneRight.length - sStoneRight; // Площадь воды = высота блока * кол-во блоков(длинна под водой) - площадь камней в воде
-	console.log('---всего воды', sWaterRight);
-	console.log("Длинна под водой" ,stoneRight.length);
-	let newArr = arr.slice(maxRightIndex, arr.length);
-		
-	console.log('навый массив',newArr);
-	 	
-	right(newArr, maxRightIndex);
-	return sWaterRight;
+    console.log("---всего воды", sWaterRight);
+    console.log("Длинна под водой", stoneRight.length);
+    let newArr = arr.slice(maxRightIndex, arr.length);
 
-	}
-//sWaterRight += sWaterRight;
+    console.log("навый массив", newArr);
+
+    right(newArr, maxRightIndex);
+    return sWaterRight;
+  }
+  //sWaterRight += sWaterRight;
 }
 right(rightArr, maxIndex);
-	console.log('+++++всего воды', sWaterRight);
 
-
-
+console.log("+++++всего воды", sWaterRight);
+console.log("++++++++++++++++++++++++++++++++");
 
 // --------------------- Левая часть--------------------------//
 
-var maxLeftVal = 0;
-var maxLeftIndex = 0;
-var sWaterLeft = 0;
-var sStoneLeft = 0; // общая площадь камней под водой
-for (let i = maxIndex - 1; i >= 0; i--) {
-  if (arr[i] > maxLeftVal) {
-    maxLeftIndex = i;
-    maxLeftVal = arr[i];
-  }
-}
-var stoneLeft = arr.slice(maxLeftIndex + 1, maxIndex); // - (массив) кол-во блоков под водой между вершинами
+function left(arr) {
+  console.log(arr);
+  let firstLeftVal = arr[0];
+  let maxLeftVal = 0;
+  let sStoneLeft = 0;
 
-for (let i = 0; i < stoneLeft.length; i++) {
-  sStoneLeft += stoneLeft[i];
+  //	console.log(maxVal);
+
+  for (let i = arr.length - 2; i >= 0; i--) {
+    if (arr[i] > maxLeftVal) {
+      maxLeftIndex = i;
+      maxLeftVal = arr[i];
+    }
+  }
+  console.log("длинна", arr.length - 1);
+  console.log("1я вершина", maxLeftIndex);
+  console.log("2я вершина", maxLeftVal);
+  let stoneLeft = arr.slice(maxLeftIndex + 1, arr.length - 1); // - (массив) кол-во секций (длинна под водой) между вершинами
+  console.log("все что под водой", stoneLeft);
+
+  for (let i = 0; i < stoneLeft.length; i++) {
+    sStoneLeft += stoneLeft[i];
+    //	console.log(sStoneRight, stoneRight[i]);
+  }
+  console.log("Сумма камней под водой", sStoneLeft);
+  if (stoneLeft.length > 0) {
+    sWaterLeft += maxLeftVal * stoneLeft.length - sStoneLeft; // Площадь воды = высота блока * кол-во блоков(длинна под водой) - площадь камней в воде
+    console.log("---всего воды", sWaterLeft);
+    console.log("Длинна под водой", stoneLeft.length);
+    let newArr = arr.slice(0, maxLeftIndex + 1);
+
+    console.log("навый массив", newArr);
+
+    left(newArr, maxLeftIndex);
+    return sWaterLeft;
+  }
+
+  sWaterLeft += sWaterLeft;
 }
-sWaterLeft = maxLeftVal * stoneLeft.length - sStoneLeft;
-console.log(sWaterLeft);
+left(leftArr);
+
+console.log("+++++всего воды", sWaterLeft);
