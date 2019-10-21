76d2cb4d243dd89cda23ea8a4de00b65345db1bd
Alexey Fedorenko <messer.ua@gmail.com>
2019-10-21 10:08:17 +0300

    Fixed

--- dz-water.js
+++ dz-water.js
@@ -1,5 +1,6 @@
 // let arr = [2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 0];
-let arr = [7, 0, 2, 8, 3, 4, 7, 2, 4, 8, 0, 11, 0, 0, 0, 3];
+// let arr = [7, 0, 2, 8, 3, 4, 7, 2, 4, 8, 0, 11, 0, 0, 0, 3];
+let arr = [0, 0, 0, 3, 4, 5, 1, 2, 9, 0, 0, 0, 5, 5, 0, 0];
 let maxVal = arr[0];
 let maxIndex = 0;
 for (let i = 0; i < arr.length; i++) {
@@ -10,7 +11,6 @@ for (let i = 0; i < arr.length; i++) {
 }
 let rightArr = arr.slice(maxIndex, maxRightIndex);
 let leftArr = arr.slice(0, maxIndex + 1);
-console.log(leftArr);
 
 var maxRightIndex = 0;
 var maxLeftIndex = 0;
@@ -39,8 +39,7 @@ function right(arr, maxIndex) {
   }
   if (stoneRight.length > 0) {
     sWaterRight += maxRightVal * stoneRight.length - sStoneRight;
-    console.log("---всего воды", sWaterRight);
-    console.log("Длинна под водой", stoneRight.length);
+    console.log("воды справа", sWaterRight);
     let newArr = arr.slice(maxRightIndex, arr.length);
     right(newArr, maxRightIndex);
     return sWaterRight;
@@ -62,22 +61,21 @@ function left(arr) {
       maxLeftVal = arr[i];
     }
   }
-
   let stoneLeft = arr.slice(maxLeftIndex + 1, arr.length - 1); // - (массив) кол-во секций (длинна под водой) между вершинами
+  console.log(stoneLeft);
 
   for (let i = 0; i < stoneLeft.length; i++) {
     sStoneLeft += stoneLeft[i];
   }
   if (stoneLeft.length > 0) {
     sWaterLeft += maxLeftVal * stoneLeft.length - sStoneLeft; // Площадь воды = высота блока * кол-во блоков(длинна под водой) - площадь камней в воде
+    console.log("воды слева", sWaterLeft);
     let newArr = arr.slice(0, maxLeftIndex + 1);
     left(newArr, maxLeftIndex);
     return sWaterLeft;
-  } else if (arr.length < 3) {
-    return;
-  }
-  sWaterLeft += sWaterLeft;
+  } else return;
+  // sWaterLeft += sWaterLeft;
 }
 left(leftArr);
 
-console.log(sWaterLeft + sWaterRight);
+console.log("Всего воды =", sWaterLeft + sWaterRight);
